279676995d9b339e021cc63764071ad3838f4a60
denzelloke <denzelloke0@gmail.com>
2024-06-02 23:53:14 +0800

    LOGIN PAGE
    created:
    - signup function
    - log in function
    - data base that stores usernames & passwords
    
    todo:
    - beautify login menu
    - beautify signup menu
    - beautify homepage

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+const collection = require("./mongodb");
+
+const templatePath = path.join(__dirname, "../templates"); //tells code that the views folder is called "templates"
+
+app.use(express.json());
+app.set("view engine", "hbs");
+app.set("views", templatePath); //tells code that the views folder is called "templates"
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+// signup form that stores name and pw into database
+app.post("/signup", async (req, res) => {
+  try {
+    const data = {
+      name: req.body.name, //fetches user input
+      password: req.body.password,
+    };
+
+    await collection.insertMany([data]); //creates a data???
+    res.render("home"); //redirect to home page
+  } catch (error) {
+    console.error("Error during signup:", error);
+    res.status(500).send("unknown error");
+  }
+});
+
+//dictates signup action
+app.post("/login", async (req, res) => {
+  //fetch user input for name and pw. if match in database then allow to render homepage
+  try {
+    const check = await collection.findOne({ name: req.body.name });
+    if (check.password === req.body.password) {
+      res.render("home"); //redirect to home page
+    } else {
+      res.send("username exists, incorrect password");
+    }
+  } catch {
+    res.send("incorrect credentials");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("port connected");
+});
---
+++ src/mongodb.js
@@ -0,0 +1,26 @@
+//database collection to connect to node.js
+
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/CramBrulee")
+.then(()=>{
+    console.log("mongoDB connected")
+})
+.catch((e)=>{
+    console.log("failed to connect")
+})
+
+const LogInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const LogInCollection=new mongoose.model("LogInCollection",LogInSchema)
+
+module.exports=LogInCollection
